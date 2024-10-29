ad54f50013510c8edea54c94ef0f26a1be4d9201
Alexis Jeanjean <alexis.jeanjean@worldline.com>
2024-10-29 18:00:30 +0100

    adjust image

--- src/app/main/components/Authentication/login/login.component.ts
+++ src/app/main/components/Authentication/login/login.component.ts
@@ -23,7 +23,6 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
     this.translateService.getTranslation().subscribe((result) => {
       this.translateValues = result.login;
-      console.log(this.translateValues);
     });
   }
 }
