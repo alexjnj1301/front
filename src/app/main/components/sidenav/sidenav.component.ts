04e934b3977019375eb83215d4440e98c1e44b2b
Alexis Jeanjean <alexis.jeanjean@worldline.com>
2024-12-11 17:25:03 +0100

    WIP on feature/translate

--- src/app/main/components/sidenav/sidenav.component.ts
+++ src/app/main/components/sidenav/sidenav.component.ts
@@ -3,6 +3,8 @@ import { MatDrawer } from '@angular/material/sidenav'
 import { SidenavService } from '../../services/sidenav.service'
 import { MultipleTransLoaderHttp } from '../../../MultipleTransLoaderHttp'
 import { AuthenticationService } from '../../services/authentication.service'
+import { CurrentUser } from '../../../models/CurrentUser'
+import { TranslateService } from '@ngx-translate/core'
 
 @Component({
   selector: 'app-sidenav',
@@ -12,15 +14,19 @@ import { AuthenticationService } from '../../services/authentication.service'
 export class SidenavComponent implements OnInit {
   @ViewChild('drawer') public drawer!: MatDrawer
   public translateValues: any = {}
+  public currentUser: CurrentUser | null = null
+  public test: string = ''
 
   constructor(protected sideNavService: SidenavService,
-              private authService: AuthenticationService,
+              protected authService: AuthenticationService,
               private translateService: MultipleTransLoaderHttp) {
   }
 
   public ngOnInit(): void {
     this.translateService.getTranslation().subscribe((result) => {
       this.translateValues = result.sidenav
+      this.test = this.translateValues.welcomeUser
+      console.log("test", this.test)
     })
     this.sideNavService.toggleSidenav$.subscribe(() => {
       this.toggleSideNav()
@@ -29,6 +35,10 @@ export class SidenavComponent implements OnInit {
 
   public toggleSideNav(): void {
     this.drawer.toggle()
+  }
+
+  public isLoggedIn(): boolean {
+    return this.authService.isAuthenticated();
   }
 
   public logout(): void {
